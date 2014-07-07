3107ae62849491883ee0313c0836dba6bb3c4dda
Harrison Muskat <harrisonmuskat@gmail.com>
2014-07-07 15:05:30 -0400

    Still fixing validation error

--- index.php
+++ index.php
@@ -30,35 +30,30 @@
 		<div class = 'col-md-10 col-md-offset-1'>
 			<h3 class = 'text-center output img-rounded'> <?php echo $pw; ?> </h3>
 			<br>
-		</div>
-
-		<p>
 			<div class = 'col-md-6 col-md-offset-3 text-center'>
-				
-					<form method = 'POST' action = 'index.php'>
-						<label for='words'>How Many Words?</label>
-							<input class = 'input-sm input-width input' type='text' name='words' id='words' maxlength = 1 value=''> 
-							<label for = 'words'>(Max. 9)</label> <br>
+				<form method = 'POST' action = 'index.php'>
+					<label for='words'>How Many Words?</label>
+						<input class = 'input-sm input-width input' type='text' name='words' id='words' maxlength = 1 value=''> 
+						<label for = 'words'>(Max. 9)</label> <br>
 
-						<label for='number'>How Many Numbers?</label>
-							<input class = 'input-sm input-width input' type='text' name='number' id='number' maxlength = 1 value=''> 
-							<label for = 'number'>(Max. 9)</label> <br>
+					<label for='number'>How Many Numbers?</label>
+						<input class = 'input-sm input-width input' type='text' name='number' id='number' maxlength = 1 value=''> 
+						<label for = 'number'>(Max. 9)</label> <br>
 
-						<label for='symbol'>How Many Symbols?</label>
-							<input class = 'input-sm input-width input' type='text' name='symbol' id='symbol' maxlength = 1 value=''> 
-							<label for = 'symbol'>(Max. 9)</label> <br>
+					<label for='symbol'>How Many Symbols?</label>
+						<input class = 'input-sm input-width input' type='text' name='symbol' id='symbol' maxlength = 1 value=''> 
+						<label for = 'symbol'>(Max. 9)</label> <br>
 
-						<label for='uppercase'>Capitalize first letter?</label>
-							<input type = 'checkbox' name = 'uppercase' id = 'uppercase'> <br>
+					<label for='uppercase'>Capitalize first letter?</label>
+						<input type = 'checkbox' name = 'uppercase' id = 'uppercase'> <br>
 
-						<label for='camelcase'>CamelCase?</label>
-							<input type = 'checkbox' name = 'camelcase' id = 'camelcase'> <br><br>
+					<label for='camelcase'>CamelCase?</label>
+						<input type = 'checkbox' name = 'camelcase' id = 'camelcase'> <br><br>
 
-						<input type='submit' class='btn btn-primary btn-lg' value='Generate Password'>
-					</form>
-				
+					<input type='submit' class='btn btn-primary btn-lg' value='Generate Password'>
+				</form>
 			</div>
-		</p>
+		</div>
 	</div>
 
 	<div class = 'row'>
