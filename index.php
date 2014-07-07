e46a200697d1a88228efa9aaad523a0de13ddecc
Harrison Muskat <harrisonmuskat@gmail.com>
2014-07-07 14:35:10 -0400

    Made it look nice

--- index.php
+++ index.php
@@ -35,19 +35,34 @@
 		<div class = 'col-md-6 col-md-offset-3 text-center'>
 			<p>
 				<form method = 'POST' action = 'index.php'>
-					<label for='words'># of Words</label>
+					<label for='words'>How Many Words?</label>
 						<input class = 'input-sm input-width input' type='text' name='words' id='words' maxlength = 1 value=''> 
 						<label for = 'words'>(Max. 9)</label> <br>
-					<label for='number'>Include a Number?</label>
-						<input type = 'checkbox' name = 'number' id = 'number'> <br>
-					<label for='symbol'>Include a Symbol?</label>
-						<input type = 'checkbox' name = 'symbol' id = 'symbol'> <br>
+
+					<label for='number'>How Many Numbers?</label>
+						<input class = 'input-sm input-width input' type='text' name='number' id='number' maxlength = 1 value=''> 
+						<label for = 'number'>(Max. 9)</label> <br>
+
+					<label for='symbol'>How Many Symbols?</label>
+						<input class = 'input-sm input-width input' type='text' name='symbol' id='symbol' maxlength = 1 value=''> 
+						<label for = 'symbol'>(Max. 9)</label> <br>
+
 					<label for='uppercase'>Capitalize first letter?</label>
-						<input type = 'checkbox' name = 'uppercase' id = 'uppercase'> <br><br>
+						<input type = 'checkbox' name = 'uppercase' id = 'uppercase'> <br>
+
+					<label for='camelcase'>CamelCase?</label>
+						<input type = 'checkbox' name = 'camelcase' id = 'camelcase'> <br><br>
 
 					<input type='submit' class='btn btn-primary btn-lg' value='Generate Password'>
 				</form>
 			</p>
+		</div>
+	</div>
+
+	<div class = 'row'>
+		<div class = 'col-md-8 col-md-offset-2'>
+			<p> Where it all began: </p>
+			<a href = 'http://xkcd.com/936/' target = '_blank'> <img src = 'http://imgs.xkcd.com/comics/password_strength.png' alt = 'XKCD 936' /> </a>
 		</div>
 	</div>
 
